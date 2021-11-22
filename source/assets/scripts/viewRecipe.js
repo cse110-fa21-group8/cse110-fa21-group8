444132aa0e1814ded7226e19898e806785bbfd78
Jonny Tran <61168867+jonathanktran@users.noreply.github.com>
2021-11-21 21:41:06 -0800

    Add recipe page button functionality

--- source/assets/scripts/viewRecipe.js
+++ source/assets/scripts/viewRecipe.js
@@ -1,12 +1,16 @@
-let favButton = document.getElementById('favBtn');
-favButton.addEventListener('click', function () 
-{
-    if (favButton.getAttribute('src') == 'assets/images/heartEmpty.png')
-    {
-        favButton.setAttribute('src', 'assets/images/heartFull.png');
+// toggle favorites button
+let favBtn = document.getElementById('favBtn');
+favBtn.addEventListener('click', function () {
+    if (favBtn.getAttribute('src') == 'assets/images/heartEmpty.png') {
+        favBtn.setAttribute('src', 'assets/images/heartFull.png');
     }
-    else 
-    {
-        favButton.setAttribute('src', 'assets/images/heartEmpty.png');
+    else {
+        favBtn.setAttribute('src', 'assets/images/heartEmpty.png');
     }
+});
+
+// move to edit recipe page
+let editBtn = document.getElementById('editBtn');
+editBtn.addEventListener('click', function () {
+    window.location = 'editRecipe.html';
 });
